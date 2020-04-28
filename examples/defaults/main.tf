9c5c95896e92986fd41409385b15149db4dd1bb1
rob <robcannonxyz@gmail.com>
2020-04-28 08:36:56 -0700

    trimming tests examle

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,21 +1,34 @@
 
-module "vpc" {
+module "network" {
   source = "github.com/insight-icon/terraform-icon-monitor-aws-network.git?ref=master"
 }
 
 module "rds" {
   source         = "github.com/insight-icon/terraform-icon-monitor-aws-rds.git?ref=master"
-  security_group = module.vpc.rds_security_group_id
-  subnet_ids     = module.vpc.public_subnets
+  security_group = module.network.rds_security_group_id
+  subnet_ids     = module.network.public_subnets
+  username       = "cachet"
+  password       = "hunter2"
+}
+
+variable "public_key" {}
+variable "private_key_path" {}
+
+output "endpoint" {
+  value = module.rds.this_db_instance_endpoint
 }
 
 module "defaults" {
-  source            = "../.."
-  db_host           = ""
-  db_password       = ""
-  db_username       = ""
-  private_key_path  = ""
-  public_key        = ""
-  security_group_id = ""
-  subnet_id         = ""
+  source      = "../.."
+  db_host     = module.rds.this_db_instance_endpoint
+  db_password = "hunter21"
+  db_username = "cachet"
+
+  private_key_path  = var.private_key_path
+  public_key        = var.public_key
+  security_group_id = module.network.cachet_security_group_id
+  subnet_id         = module.network.public_subnets[0]
+
+  root_domain_name = "blockstatus.net"
+  hostname         = "ci"
 }
--- test/terraform_defaults_test.go
+++ test/terraform_defaults_test.go
@@ -1,26 +1,64 @@
 package test
 
 import (
+	"fmt"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"io/ioutil"
+	"log"
+	"path"
 	"testing"
 )
 
-func TestTerraformDefaults(t *testing.T) {
+func TestDefault(t *testing.T) {
 	t.Parallel()
 
 	exampleFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/defaults")
 
-	terraformOptions := &terraform.Options{
-		TerraformDir: exampleFolder,
-		Vars: map[string]interface{}{},
-	}
-
 	defer test_structure.RunTestStage(t, "teardown", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, exampleFolder)
 		terraform.Destroy(t, terraformOptions)
+
+		keyPair := test_structure.LoadEc2KeyPair(t, exampleFolder)
+		aws.DeleteEC2KeyPair(t, keyPair)
 	})
 
 	test_structure.RunTestStage(t, "setup", func() {
+		terraformOptions, keyPair := configureTerraformOptions(t, exampleFolder)
+		test_structure.SaveTerraformOptions(t, exampleFolder, terraformOptions)
+		test_structure.SaveEc2KeyPair(t, exampleFolder, keyPair)
+
 		terraform.InitAndApply(t, terraformOptions)
 	})
 }
+
+
+func configureTerraformOptions(t *testing.T, exampleFolder string) (*terraform.Options, *aws.Ec2Keypair) {
+
+	uniqueID := random.UniqueId()
+
+	keyPairName := fmt.Sprintf("ci-node-%s", uniqueID)
+	keyPair := aws.CreateAndImportEC2KeyPair(t, "us-east-1", keyPairName)
+
+	privateKeyPath := path.Join(exampleFolder, keyPairName)
+
+	err := ioutil.WriteFile(privateKeyPath, []byte(keyPair.PrivateKey), 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Key saved to: %s", privateKeyPath)
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: exampleFolder,
+		Vars: map[string]interface{}{
+			"public_key":    keyPair.PublicKey,
+			"private_key_path": privateKeyPath,
+		},
+	}
+
+	return terraformOptions, keyPair
+}
+
